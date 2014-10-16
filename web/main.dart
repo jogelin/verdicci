edc54844bba592e7306c2e2fd7cbc5fcd7e9f0a9
Jo <gelin_jo@hotmail.com>
2014-10-16 19:37:19 +0200

    fix ctrl + firefox

--- web/main.dart
+++ web/main.dart
@@ -3,32 +3,21 @@ library verdicci;
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
-import 'package:verdicci/google-map/maps.dart';
 import 'package:verdicci/service/query_service.dart';
 import 'package:verdicci/menu/menu_component.dart';
 
-import 'package:verdicci/welcome/welcome_controller.dart';
 import 'package:verdicci/welcome/news_component.dart';
 
-import 'package:verdicci/style/style_controller.dart';
 import 'package:verdicci/style/styles_component.dart';
-
-import 'package:verdicci/openhours/openhours_controller.dart';
-import 'package:verdicci/contact/contact_controller.dart';
 
 import 'package:verdicci/routing/app_router.dart';
 
 class VerdicciAppModule extends Module {
   VerdicciAppModule() {
-    bind(WelcomeController);
-    bind(StyleController);
-    bind(OpenHoursController);
-    bind(ContactController);
 
     bind(MenuComponent);
     bind(NewsComponent);
     bind(StylesComponent);
-    bind(XGoogleMaps);
     bind(QueryService);
 
     bind(RouteInitializer, toImplementation: AppRouteInitializer );
